842fe8aa89e59af47ff32c16d254ff30342928cd
wajd alkayyali <wajd.kayyali@gmail.com>
2023-10-21 22:26:54 +0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../index.css";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkStyle = {
+    display: "flex",
+    gap: "10px",
+  };
+  return (
+    <nav>
+      <div style={linkStyle}>
+        {links.map((link, index) => (
+          <div key={index}>
+            <a href={`#${link}`}>{link}</a>
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
